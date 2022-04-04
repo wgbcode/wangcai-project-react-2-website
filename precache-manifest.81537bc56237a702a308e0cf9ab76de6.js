3f61e5bc0e1ef77ab23e58bf7df9f4e1f35990a8
wudada <2567199685@qq.com>
2022-04-04 11:44:55 +0800

    deploy

---
+++ precache-manifest.81537bc56237a702a308e0cf9ab76de6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5bcf7016b7f72ec1aef07270f951ac3",
+    "url": "/wangcai-project-react-2-website/index.html"
+  },
+  {
+    "revision": "1c478bebda1379f24bf8",
+    "url": "/wangcai-project-react-2-website/static/css/main.b586f6e1.chunk.css"
+  },
+  {
+    "revision": "a84d471a08317fb3ba41",
+    "url": "/wangcai-project-react-2-website/static/js/2.a94197f0.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/wangcai-project-react-2-website/static/js/2.a94197f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c478bebda1379f24bf8",
+    "url": "/wangcai-project-react-2-website/static/js/main.5db88b1c.chunk.js"
+  },
+  {
+    "revision": "3bbf3d1443c7c441a4e5",
+    "url": "/wangcai-project-react-2-website/static/js/runtime-main.94f4a8d7.js"
+  }
+]);
